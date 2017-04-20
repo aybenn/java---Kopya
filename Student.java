ee4b92204aa4dc3b50b11abf3d8b998935686433
aybenn <aybenduyan22@gmail.com>
2017-04-20 23:03:42 +0300

    versıyon1
    javaproje

---
+++ Student.java
@@ -0,0 +1,15 @@
+
+public class Student {
+
+	String sname,ssurname;
+	double cgpa;
+	int age;
+	long stdno;
+	Student(String sname,String surname,double d,int a, long stdn){
+		this.sname = sname;
+		this.ssurname = surname;
+		this.age = a;
+		this.cgpa = d;
+		this.stdno = stdn;
+	}
+}
